0fcfdc0360eb6b14b91c5ac1ecb4b63692a1b9b2
Bcpratt2000 <bcpratt2000@gmail.com>
2017-10-10 12:43:07 -0600

    reduced resolution of colors to randomise in order to have more diverse colors

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        view.backgroundColor = randomColor()
+    }
+    
+    private func randomColor() -> UIColor{
+        return UIColor(red: CGFloat(Float(arc4random()%8)/8.0), green: CGFloat(Float(arc4random()%8)/8.0), blue: CGFloat(Float(arc4random()%8)/8.0), alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = randomColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
