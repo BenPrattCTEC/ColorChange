9c633f141c726f08bbf6268ea4a2c2d2db0cbf53
Bcpratt2000 <bcpratt2000@gmail.com>
2017-10-18 12:51:19 -0600

    made the picture changer work, currently working on Sound

--- ColorChange/Controller/MediaController.swift
+++ ColorChange/Controller/MediaController.swift
@@ -11,6 +11,43 @@ import UIKit
 public class MediaController: UIViewController{
     
     
+    @IBOutlet weak var imageViewer: UIImageView!
+    @IBOutlet weak var soundButton: UIButton!
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    private var imageCounter = 0
+    
+    
+    @IBAction func imageAction(_ sender: UIButton) {
+        changeImage()
+    }
+    
+    @IBAction func soundAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func volumeSliderAction(_ sender: UISlider) {
+    }
+    
+    private func playSound(){
+    }
+    
+    private func changeImage(){
+        imageCounter+=1
+        if(imageCounter>=3){
+            imageCounter = 0;
+        }
+        
+        if(imageCounter == 0){
+            imageViewer.image = UIImage(named: "MarioSprite")
+        }
+        else if(imageCounter == 1){
+            imageViewer.image = UIImage(named: "LuigiSprite")
+        }
+        else{
+            imageViewer.image = UIImage(named: "MarioHead")
+        }
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
--- ColorChange/Model/ColorTools.swift
+++ ColorChange/Model/ColorTools.swift
@@ -11,7 +11,8 @@ import UIKit
 
 public class ColorTools{
     
-    public static var lastColor: UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    public static var lastColor: UIColor = UIColor(red: CGFloat(Float(arc4random()%8)/8.0), green: CGFloat(Float(arc4random()%8)/8.0), blue: CGFloat(Float(arc4random()%8)/8.0), alpha: 1)
+                                            //UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     
     public static func randomColor() -> UIColor{
         lastColor = UIColor(red: CGFloat(Float(arc4random()%8)/8.0), green: CGFloat(Float(arc4random()%8)/8.0), blue: CGFloat(Float(arc4random()%8)/8.0), alpha: 1)
